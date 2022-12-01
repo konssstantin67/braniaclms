5a5de0042ee06f5748c1ca404a392ec4d0335a15
konssstantin67 <kossstantin67@gmail.com>
2022-12-01 18:10:27 +0500

    day2.1

---
+++ views.py
@@ -0,0 +1,25 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+class MainPageView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class NewsPageView(TemplateView):
+    template_name = 'mainapp/news.html'
+
+
+class CoursesPageView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+
+class ContactsPageView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+
+class DocSitePageView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+
+class LoginPageView(TemplateView):
+    template_name = 'mainapp/login.html'
