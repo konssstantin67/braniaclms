4c5e9e59fa395e0c95f6061a68ac5c47c1b9af32
konssstantin67 <kossstantin67@gmail.com>
2022-12-02 12:57:37 +0500

    lesson_3

--- urls.py
+++ urls.py
@@ -6,10 +6,7 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', views.MainPageView.as_view()),
-    path('news/', views.NewsPageView.as_view()),
-    path('courses/', views.CoursesPageView.as_view()),
-    path('contacts/', views.ContactsPageView.as_view()),
-    path('doc_site/', views.DocSitePageView.as_view()),
-    path('login/', views.LoginPageView.as_view()),
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='mainapp/')),
+    path('maimapp/', include('mainapp.urls')),
 ]
